73d6f46e15f28bb8138b71cabfe1052539060928
YannickSCF <yannickscf@gmail.com>
2023-04-15 20:38:12 +0200

    Updated packages

---
+++ Assets/0_ProjectAssets/2_Scripts/1_Controllers/GameManager/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YannickSCF.GeneralApp.GameManager;
+
+public class GameManager : BaseGameManager {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
